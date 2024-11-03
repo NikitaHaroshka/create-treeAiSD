756fd55a03be124f8874b39c2933b45f6e2ab7cd
NikitaHaroshka <nikiharoshka@gmail.com>
2024-11-03 20:09:22 +0300

    Read-write

---
+++ Tree1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <fstream>
+#include <set>
+#include <vector>
+using namespace std;
+int main()
+{
+    ifstream inputFile("input.txt");
+    ofstream outputFile("output.txt");
+    if (!inputFile) {
+        cerr << "Ошибка открытия входного файла!" << endl;
+        return 1;
+    }
+    if (!outputFile) {
+        cerr << "Ошибка открытия выходного файла!" << endl;
+        return 1;
+    }
+    
+
+
+    inputFile.close();
+    outputFile.close();
+
+    return 0;
+}
